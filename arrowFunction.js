32fc88b4d2e442377a3db06de9506345a9b3581c
Shovan-Banik <shovan15-7790@diu.edu.bd>
2023-02-22 02:05:48 +0600

    map function done

---
+++ arrowFunction.js
@@ -0,0 +1,26 @@
+// 3.1
+// const division=div=>div/5;
+// console.log(division(100));
+
+
+
+// 3.2
+// const mul=(first,second)=>(first+2)*(second+2);
+// console.log(mul(5,5));
+
+
+// 3.3
+// const multi=(a,b,c)=>a*b*c;
+// console.log(multi(5,5,5));
+
+
+
+
+// 3.4
+// const mul=(first,second)=>{
+//     const sum1=(first+2);
+//     const sum2=(second+2);
+//     return sum1*sum2;
+// }
+// console.log(mul(5,5));
+
---
+++ map.js
@@ -0,0 +1,2 @@
+const numbers=[2,3,4,5,6,7,8,9].map(n=>n*5);
+console.log(numbers);
