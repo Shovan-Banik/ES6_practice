8b9d9c3daba93822fa4196f822566b6f3981f080
Shovan-Banik <shovan15-7790@diu.edu.bd>
2023-02-22 01:38:19 +0600

    templete string for object

---
+++ templet-boject.js
@@ -0,0 +1,9 @@
+const person={
+    name:'shovan',
+    age: 27,
+    job: 'bakar'
+}
+const multiple=`my name is ${person.name}
+my age ${person.age}
+my job is: ${person.job}`;
+console.log(multiple);
