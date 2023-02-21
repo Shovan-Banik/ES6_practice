4e8d9edce7fb45eb346d89f489cc8112f28c56c8
Shovan-Banik <shovan15-7790@diu.edu.bd>
2023-02-22 02:28:58 +0600

    find ar problem solve done

---
+++ find.js
@@ -0,0 +1,9 @@
+const phone=[
+    {name:'iphone', price:50000},
+    {name:'nokia', price:25000},
+    {name:'xiomi', price:17000},
+    {name:'oppo', price:12000},
+    {name:'walton', price:5000},
+    {name:'symphony', price:5500},
+].find(x=>x.price===5000);
+console.log(phone);
